45e278935d3560bcdb4867146c596cff549145b0
Katsuma Ito <katsumai@gmail.com>
2021-08-19 14:51:50 +0900

    feat: fetch message from /api/hello

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,10 +1,38 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
-export default (req: NextApiRequest, res: NextApiResponse<Data>) => {
-  res.status(200).json({ name: 'John Doe' })
-}
+const handler = (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  res.status(200).json({ name: "World" });
+};
+
+export default handler;
+
+export type FetchResult =
+  | {
+      result: Data;
+      error: null;
+    }
+  | {
+      result: null;
+      error: object;
+    }
+  | {
+      result: null;
+      error: null;
+    };
+
+export const fetchHello = (): Promise<FetchResult> => {
+  return fetch("/api/hello")
+    .then((r) => {
+      if (r.status === 200) return r;
+      console.error(r);
+      throw new Error("invalid status code");
+    })
+    .then((r) => r.json())
+    .then((resp) => ({ result: resp, error: null }))
+    .catch((e) => ({ result: null, error: e }));
+};
